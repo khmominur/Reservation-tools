ace9bd263228840f1fa11a3f5c6299020238edd7
Mominur Rahman <150646318+khmominur@users.noreply.github.com>
2026-04-20 12:13:03 +0600

    Prank add

--- PranakScript.js
+++ PranakScript.js
@@ -1,6 +1,25 @@
-function calculateFare() {
-    document.body.style.opacity = "0.5"; // fade effect
-    setTimeout(() => {
-        window.location.href = "prank.html";
-    }, 300);
-}
+// redirect to prank html
+
+// function calculateFare() {
+//     document.body.style.opacity = "0.5"; // fade effect
+//     setTimeout(() => {
+//         window.location.href = "prank.html";
+//     }, 300);
+// }
+
+
+
+
+// prank (Random Position)
+    const btn = document.getElementById("runBtn");
+
+btn.addEventListener("mouseover", () => {
+    btn.style.position = "absolute";
+
+    // random position (screen এর ভিতরে)
+    const x = Math.random() * (window.innerWidth - btn.offsetWidth);
+    const y = Math.random() * (window.innerHeight - btn.offsetHeight);
+
+    btn.style.left = x + "px";
+    btn.style.top = y + "px";
+});
